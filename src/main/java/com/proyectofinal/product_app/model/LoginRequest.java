5bf15befb6e1f186af8cc7c8cc4c835a3bf3f62c
Sammy-sky <programacion0205@gmail.com>
2026-02-25 18:38:39 -0500

    fix: remove @Entity from LoginRequest

--- src/main/java/com/proyectofinal/product_app/model/LoginRequest.java
+++ src/main/java/com/proyectofinal/product_app/model/LoginRequest.java
@@ -1,21 +1,13 @@
 package com.proyectofinal.product_app.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 @Data
 public class LoginRequest {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
     private String username;
     private String password;
-
 }
